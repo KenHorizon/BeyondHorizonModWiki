8f00bf9485068ac7222cb036038e722c277fb30f
KenHorizon <vincentruales845@gmail.com>
2024-09-14 08:21:13 +0800

    Cleaning codes

---
+++ assets/js/latest_support_version.js
@@ -0,0 +1,5 @@
+mod_name = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("mod_name").innerHTML = value.name)
+version = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("version").innerHTML = value.version)
+mod_loader_name = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("mod_loader_name").innerHTML = value.mod_loader_name)
+mod_loader_version = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("mod_loader_version").innerHTML = value.mod_loader_version)
+minecraft_version = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("minecraft_version").innerHTML = value.minecraft_version)
