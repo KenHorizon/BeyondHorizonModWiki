29a0bd4c8e0acc3085387a97d6da25f938af16be
Admin <kenhorizon7@gmail.com>
2024-10-15 13:02:24 +0800

    Fixing Some Errors

--- assets/js/latest_support_version.js
+++ assets/js/latest_support_version.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 mod_name = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("name").innerHTML = value.name)
-=======
-mod_name = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("mod_name").innerHTML = value.name)
->>>>>>> 186bf42e9a4020dc82d43384f5a03e2785924cad
 version = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("version").innerHTML = value.version)
 mod_loader_name = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("mod_loader_name").innerHTML = value.mod_loader_name)
 mod_loader_version = fetch("assets/json/latest_support_version.json").then(response => response.json()).then(value => document.getElementById("mod_loader_version").innerHTML = value.mod_loader_version)
